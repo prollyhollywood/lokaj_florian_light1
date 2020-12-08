293f9313c633270b1901ae25bc3aaf07756819d7
prollyhollywood <72751673+prollyhollywood@users.noreply.github.com>
2020-12-08 11:03:32 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+x = input.light_level()
 while True:
-    print("light level: " + input.light_level())
+    print("Light Level:" + input.light_level())
+    if x > 1:
+        light.set_all(light.rgb(255,0,0))
+    else:
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let x = input.lightLevel()
 while (true) {
-    console.log("light level: " + input.lightLevel())
+    console.log("Light Level:" + input.lightLevel())
+    if (x > 1) {
+        light.setAll(light.rgb(255, 0, 0))
+    } else {
+        light.clear()
+    }
+    
 }
